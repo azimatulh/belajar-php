cdf133339cd4f9dc8dd797c64656859ca0617d63
Azimatul Hanafiyah <afiya611@gmail.com>
2022-10-24 12:21:44 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+    echo "Hello ges!<br>";
+
+    $nama = "Azimatul Hanafiyah";
+    $umur = 21;
+
+    echo "Nama saya <strong>$nama</strong> , saya berusia <strong>$umur</strong> tahun.<br>";
+
+    $namaKakak = "Inke Arysanthi";
+    $umurKakak = 31;
+
+    echo "Nama kakak saya <strong>$namaKakak</strong> , kakak saya saya berusia <strong>$umurKakak</strong> tahun.<br>";
+
+    $selisihUmur = $umurKakak - $umur;
+    echo " Selisih usia saya dan kakak saya adalah <strong>$selisihUmur</strong> tahun."
+
+
+
+
+?>
